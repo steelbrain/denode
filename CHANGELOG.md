75e5bfe80eda6a08ac466a69bccb72d41e1b220c
steelbrain <de.steelbrain@gmail.com>
2016-04-15 23:47:47 -0700

    :new: Show only the dev tools, not the window

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## 1.0.1
+
+- Show only the dev tools, not the whole window
+
+## 1.0.0
+
+- Initial release
--- denode/index.js
+++ denode/index.js
@@ -4,9 +4,18 @@ const Path = require('path')
 const Electron = require('electron')
 
 Electron.app.on('ready', function() {
-  var win = new Electron.BrowserWindow({ width: 800, height: 600, show: true });
+  var win = new Electron.BrowserWindow({ width: 800, height: 600 });
   win.loadURL('file://' + Path.join(__dirname, 'index.html') + '?app=' + encodeURIComponent(process.argv[2] || ''));
   win.on('closed', function() {
+    Electron.app.quit()
+  })
+  win.webContents.openDevTools({ detach: true })
+  win.webContents.once('devtools-opened', function() {
+    setImmediate(function() {
+      win.hide()
+    })
+  })
+  win.webContents.once('devtools-closed', function() {
     Electron.app.quit()
   })
 })
